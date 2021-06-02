aedb6e19bff45d8c7021524890118a59aec21bb2
Kevin Sidlacan <sidlacankevindale@gmail.com>
2021-06-02 17:25:20 +0800

    Test for heroku

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -29,15 +29,15 @@ export default function MainNav() {
     </>
   ) : (
     <>
-      <Nav.Link onClick={handleClick}>Logout</Nav.Link>
+      <Nav.Link as={Link} to="/" onClick={handleClick}>
+        Logout
+      </Nav.Link>
     </>
   );
 
   return (
     <Navbar bg="light" expand="md">
-      <Navbar.Brand as={Link} to="/">
-        BudgetTracker
-      </Navbar.Brand>
+      <Navbar.Brand>BudgetTracker</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">{navLinks}</Nav>
